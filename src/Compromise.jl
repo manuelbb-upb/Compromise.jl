dd524fb79d8ca2d5539b9e28bbf2dc4cb5abfad4
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2024-01-16 19:36:18 +0100

    move using Requires top scope

--- src/Compromise.jl
+++ src/Compromise.jl
@@ -60,8 +60,8 @@ using .NonlinearFunctions
 
 # Import the optional extension `ForwardDiffBackendExt`, if `ForwardDiff` is available:
 
+using Requires
 @static if !isdefined(Base, :get_extension)
-    using Requires
     function __init__()
         @require ForwardDiff="f6369f11-7733-5829-9624-2563aa707210" begin
             include("../ext/ForwardDiffBackendExt/ForwardDiffBackendExt.jl")
