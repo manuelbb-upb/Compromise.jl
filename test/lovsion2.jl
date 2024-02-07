bc27d414fbc78967b7dede2c11d37c2eab4d43a7
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2024-02-07 16:32:22 +0100

    Fixes in Unscaling + HiGHS
    This commit does two things:
    1) It changes `DEFAULT_QP_OPTIMIZER` to `HiGHS.Optimizer` and adds the
       deps.
    2) It fixes some unscaling issues with bound constraints. I don't know
       if I caught them all, we need more tests.

---
+++ test/lovsion2.jl
@@ -0,0 +1,30 @@
+using Compromise
+import ForwardDiff as FD
+using Test
+let
+mop = MutableMOP(; 
+    num_vars=2,
+    lb = fill(-.5, 2),
+    ub = [0.0, 0.5]
+)
+objf!(y, x) = begin
+    y[1] = x[2]
+    y[2] = - (x[2]-x[1]^3)/(x[1]+1)
+    nothing
+end
+add_objectives!(
+    mop, objf!, :rbf; 
+    dim_out=2, func_iip=true, backend=ForwardDiffBackend()
+)
+
+algo_opts = AlgorithmOptions(;
+    eps_crit=1e-3,
+    stop_delta_min=1e-12, 
+    stop_max_crit_loops=10,
+)
+x0 = [
+    -0.125,
+    -0.3888888889,
+]
+fv, r = optimize(mop, x0; algo_opts)
+end
