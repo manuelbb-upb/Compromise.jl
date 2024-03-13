c5fc6b3d3a6d643117c87172807420ff7f171e73
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2024-03-13 14:33:53 +0100

    batch scaling

--- test/multifunc.jl
+++ test/multifunc.jl
@@ -23,7 +23,7 @@ xopt = opt_vars(ret)
 @test !oop_mat_func_called[]
 #%%
 mop = MutableMOP(; num_vars=2)
-add_objectives!(mop, multiobjf, :rbf; dim_out=2, func_iip=false, func_is_multi=true)
+add_objectives!(mop, multiobjf, :rbf; dim_out=2, func_iip=false, chunk_size=Inf)
 
 oop_mat_func_called[] = false
 ret = optimize(mop, [π, -ℯ])
@@ -39,8 +39,11 @@ function multiobjf(y::AbstractVector, x::AbstractVector)
 end
 
 iip_mat_func_called = Ref(false)
+iip_sx = Ref(0)
 function multiobjf(y::AbstractMatrix, x::AbstractMatrix) 
     global iip_mat_func_called[] = true
+    global iip_sx
+    iip_sx[] = max(size(x, 2), iip_sx[])
     map(multiobjf, eachcol(y), eachcol(x))
     nothing
 end
@@ -55,7 +58,7 @@ xopt = opt_vars(ret)
 @test !iip_mat_func_called[]
 #%%
 mop = MutableMOP(; num_vars=2)
-add_objectives!(mop, multiobjf, :rbf; dim_out=2, func_iip=true, func_is_multi=true)
+add_objectives!(mop, multiobjf, :rbf; dim_out=2, func_iip=true, chunk_size=Inf)
 
 iip_mat_func_called[] = false
 ret = optimize(mop, [π, -ℯ]);
@@ -84,10 +87,32 @@ function parallel_objf(x)
 end
 #%%
 mop = MutableMOP(; num_vars=2)
-add_objectives!(mop, parallel_objf, :rbf; dim_out=2, func_iip=false, func_is_multi=true)
+add_objectives!(mop, parallel_objf, :rbf; dim_out=2, func_iip=false, chunk_size=Inf)
 
 was_parallel[] = false
 ret = optimize(mop, [π, -ℯ]);
 xopt = opt_vars(ret)
 @test isapprox(xopt[1], xopt[2]; rtol=1e-4)
 @test was_parallel[] = true
+#%%
+mop = MutableMOP(; num_vars=2, lb = [-4.0, -4.0], ub = [4.0, 4.0])
+
+add_objectives!(mop, multiobjf, :rbf; dim_out=2, func_iip=true, chunk_size=Inf)
+
+iip_sx[] = 0
+iip_mat_func_called[] = false
+ret = optimize(mop, [π, -ℯ]);
+xopt = opt_vars(ret)
+@test isapprox(xopt[1], xopt[2]; rtol=1e-4)
+@test iip_mat_func_called[]
+@test iip_sx[] <= 3
+#%%
+mop = MutableMOP(; num_vars=2, lb = [-4.0, -4.0], ub = [4.0, 4.0])
+
+add_objectives!(mop, multiobjf, :rbf; dim_out=2, func_iip=false, chunk_size=Inf)
+
+oop_mat_func_called[] = false
+ret = optimize(mop, [π, -ℯ]);
+xopt = opt_vars(ret)
+@test isapprox(xopt[1], xopt[2]; rtol=1e-4)
+@test oop_mat_func_called[]
